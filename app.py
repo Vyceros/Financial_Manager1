74dbb9ad9b5668df5a97d9733a39f0b3d9e9ca5e
Ceprez <davidgaurg04@gmail.com>
2023-06-15 23:52:26 +0400

    commit 3

--- app.py
+++ app.py
@@ -2,16 +2,34 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, validators
-import models
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-data_transaction = models.Transaction
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = 'supersecretkey'
 
 db = SQLAlchemy(app)
+
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float, nullable=False)
+    trans_type = db.Column(db.String(10), nullable=False)
+    trans_cat = db.Column(db.String(50), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "value": self.value,
+            "trans_type": self.trans_type,
+            "trans_cat": self.trans_cat
+        }
+
+
 app.debug = True
 
 
@@ -37,11 +55,17 @@ def insert_transaction():
     value = request.form.get('value')
     trans_type = request.form.get('type')
     trans_cat = request.form.get('category')
-    new_transaction = models.Transaction(value=value, trans_type=trans_type, trans_cat=trans_cat)
+    new_transaction = Transaction(value=value, trans_type=trans_type, trans_cat=trans_cat)
     db.session.add(new_transaction)
     db.session.commit()
-    transactions = data_transaction.query.all()
 
+    transactions = [transi.to_dict() for transi in Transaction.query.all()]
+    return render_template('index.html', transactions=transactions)
+
+
+@app.route('/transactions', methods=['POST', 'GET'])
+def show_transactions():
+    transactions = [trans.to_dict() for trans in Transaction.query.all()]
     return render_template('database.html', transactions=transactions)
 
 
--- models.py
+++ models.py
@@ -12,5 +12,13 @@ class Transaction(db.Model):
     trans_type = db.Column(db.String(10), nullable=False)
     trans_cat = db.Column(db.String(50), nullable=False)
 
-    def __repr__(self):
-        return f"Transaction(id={self.id}, value={self.value}, type={self.trans_type}, category={self.trans_cat})"
+    # def __repr__(self):
+    #     return f"Transaction(id={self.id}, value={self.value}, type={self.trans_type}, category={self.trans_cat})"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "value": self.value,
+            "trans_type": self.trans_type,
+            "trans_cat": self.trans_cat
+        }
