77c39afb9df664f9b0342b80b16a094a16fe9afe
Nikusha <nikoloz.svanidze.1@btu.edu.ge>
2023-06-17 14:23:35 +0400

    flask elementebi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, redirect
+from flask import url_for, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, validators
